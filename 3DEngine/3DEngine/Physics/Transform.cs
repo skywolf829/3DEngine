696e86a581df4daf36e90c819cc7e924ab8e045e
Ethan Andersen <ethanandersen@outlook.com>
2017-03-14 17:56:30 -0400

    Added rotation matrix to transform class

--- 3DEngine/3DEngine/Physics/Transform.cs
+++ 3DEngine/3DEngine/Physics/Transform.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace _3DEngine.Physics
 {
-    class Transform
+    public class Transform
     {
+        public Matrix eulerRotationMatrix(Vector3 eulerAngles)
+        {
+            //euler.X = alpha, euler.Y = beta, euler.Z = gamme
+            float sinAlpha = (float)Math.Sin(eulerAngles.X);
+            float cosAlpha = (float)Math.Cos(eulerAngles.X);
+
+            float sinBeta = (float)Math.Sin(eulerAngles.Y);
+            float cosBeta = (float)Math.Cos(eulerAngles.Y);
+
+            float sinGamma = (float)Math.Sin(eulerAngles.Z);
+            float cosGamma = (float)Math.Cos(eulerAngles.Z);
+
+
+            Vector4 row1 = new Vector4(cosBeta*cosGamma, cosGamma * sinAlpha * sinBeta - cosAlpha * sinGamma,cosAlpha * cosGamma * sinBeta + sinAlpha * sinGamma, 0);
+            Vector4 row2 = new Vector4(cosBeta * sinGamma, cosAlpha * cosGamma + sinAlpha * sinBeta * sinGamma, (-1f) * cosGamma * sinAlpha +cosAlpha * sinBeta * sinGamma, 0);
+            Vector4 row3 = new Vector4((-1f) * sinBeta, cosBeta * sinAlpha, cosAlpha * cosBeta, 0);
+            Vector4 row4 = new Vector4(0,0,0,1);
+            Matrix result = new Matrix(row1,row2,row3,row4);
+            return (result);
+        }
     }
 }
